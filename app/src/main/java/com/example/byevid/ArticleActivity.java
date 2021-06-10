bf00023ad1d01f80c5991d564bf02d4535c96ff1
Hanifa Putri <rahimahanifa@gmail.com>
2021-06-10 22:19:41 +0700

    add article

--- app/src/main/java/com/example/byevid/ArticleActivity.java
+++ app/src/main/java/com/example/byevid/ArticleActivity.java
@@ -75,12 +75,12 @@ public class ArticleActivity extends AppCompatActivity {
     
     private void addData() {
         articleList.add(new Article("Seberapa Pentingkah Vaksinasi?", "7", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/covid_vaccine.png?alt=media&token=abb4f899-b17a-426c-b24a-28ee119fb5e4","http://google.com/"));
-        articleList.add(new Article("Penerapan Social Distancing", "7", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/social-distancing.jpeg?alt=media&token=475933d2-aebd-427d-b0f3-77468ea625db","http://google.com/"));
-        articleList.add(new Article("Tips WFH Selama Masa Pandemi", "7", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/work-from-home.jpeg?alt=media&token=9a135864-3b0d-4181-8ae8-1e394e81a784","http://google.com/"));
-        articleList.add(new Article("Jenis Masker Anti COVID-19 yang Efektif", "7", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/kids-medical-mask.jpeg?alt=media&token=ec14c6ea-92f1-4362-99e0-97ab2ac37916","http://google.com/"));
-        articleList.add(new Article("Seberapa Pentingkah Vaksinasi?", "7", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/covid_vaccine.png?alt=media&token=abb4f899-b17a-426c-b24a-28ee119fb5e4","http://google.com/"));
-        articleList.add(new Article("Penerapan Social Distancing", "7", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/social-distancing.jpeg?alt=media&token=475933d2-aebd-427d-b0f3-77468ea625db","http://google.com/"));
-        articleList.add(new Article("Tips WFH Selama Masa Pandemi", "7", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/work-from-home.jpeg?alt=media&token=9a135864-3b0d-4181-8ae8-1e394e81a784","http://google.com/"));
-        articleList.add(new Article("Jenis Masker Anti COVID-19 yang Efektif", "7", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/kids-medical-mask.jpeg?alt=media&token=ec14c6ea-92f1-4362-99e0-97ab2ac37916","http://google.com/"));
+        articleList.add(new Article("Penerapan Social Distancing", "6", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/social-distancing.jpeg?alt=media&token=475933d2-aebd-427d-b0f3-77468ea625db","http://google.com/"));
+        articleList.add(new Article("Tips WFH Selama Masa Pandemi", "5", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/work-from-home.jpeg?alt=media&token=9a135864-3b0d-4181-8ae8-1e394e81a784","http://google.com/"));
+        articleList.add(new Article("Jenis Masker Anti COVID-19 yang Efektif", "6", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/kids-medical-mask.jpeg?alt=media&token=ec14c6ea-92f1-4362-99e0-97ab2ac37916","http://google.com/"));
+        articleList.add(new Article("Cara Mencuci Tangan yang Efektif", "4", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/wash-hands.png?alt=media&token=9623d69d-8ddd-44db-b427-8864c076413a","http://google.com/"));
+        articleList.add(new Article("Tips Pintar Hindari Bakteri dan Virus", "5", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/fight-viruses.png?alt=media&token=2a3b093c-935d-4b26-943b-701f144c6266","http://google.com/"));
+        articleList.add(new Article("Perkembangan Mutasi Virus COVID-19", "4", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/covid-mutation.png?alt=media&token=31454e56-f06f-400e-8fde-767bb5be88b6","http://google.com/"));
+        articleList.add(new Article("Yuk Kenali Jenis Vaksin di Indonesia", "3", "https://firebasestorage.googleapis.com/v0/b/byevid-1cb2e.appspot.com/o/vaccine-type.png?alt=media&token=a7385316-d5c9-4d04-9a83-fd3b663d2f79","http://google.com/"));
     }
 }
