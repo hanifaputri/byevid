529aa6f86e4154e98d157efe6c3c5de47c0221c1
Hanifa Putri <rahimahanifa@gmail.com>
2021-06-10 08:52:32 +0700

    fix naming convention for activity class

--- app/src/main/java/com/example/byevid/ForgotPassActivity.java
+++ app/src/main/java/com/example/byevid/ForgotPassActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ForgotPass extends AppCompatActivity {
+public class ForgotPassActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
